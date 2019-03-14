627095a5e7798123e427a3c43182398f35868ea8
Yordan Yordanov <yordan.yordanov@dormakaba.com>
2019-03-14 15:44:37 +0200

    all needed components added No logic yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DisplayNotesComponent } from './display-notes/display-notes.component';
-import { SingleNoteComponent } from './display-notes/single-note/single-note.component';
 import { AddEditComponent } from './add-edit/add-edit.component';
 import { DisplaySingleNoteComponent } from './display-single-note/display-single-note.component';
 
@@ -14,7 +13,6 @@ import { DisplaySingleNoteComponent } from './display-single-note/display-single
     AppComponent,
     HeaderComponent,
     DisplayNotesComponent,
-    SingleNoteComponent,
     AddEditComponent,
     DisplaySingleNoteComponent
   ],
