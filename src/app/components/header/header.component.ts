f61eca864752972f65a9d3dda804358a07f597f8
Yordan Yordanov <yyordtc@training.local>
2019-03-19 20:48:08 +0200

    last push

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  searchText: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSearch(event: any):void {
+    this.searchText = event.target.value;
+    console.log( this.searchText )
   }
 
 }
