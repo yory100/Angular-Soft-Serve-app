4d55cd94edb49a8ad74a14ce66b3b79a1e114916
Yordan Yordanov <yyordtc@training.local>
2019-03-21 20:09:41 +0200

    addites animations - next animate deleting note

--- src/app/animations/route-animation.ts
+++ src/app/animations/route-animation.ts
@@ -19,6 +19,22 @@ export const slideInAnimation =
             ])
         ]),
 
+        transition('* => home', [
+            query(':enter, :leave',
+                style({ position: 'fixed', width: '100%' }),{ optional: true }),
+            group([
+                query(':enter', [
+                    style({ transform: 'translateX(100%)' }),
+                    animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+                ], { optional: true }),
+                query(':leave', [
+                    style({ transform: 'translateX(0%)' }),
+                    animate('0.5s ease-in-out',
+                        style({ transform: 'translateX(-100%)' }))
+                ], { optional: true }),
+            ])
+        ]),
+
         transition('details => *', [
             query(':enter, :leave',
                 style({ position: 'fixed', width: '100%' }),{ optional: true }),
